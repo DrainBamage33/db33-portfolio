12c14b2d21923ddaccd2059ea0da159c21ada27c
Yash <ghaghadayash33@gmail.com>
2024-06-02 23:52:31 +0530

    first commit - golang server

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func helloWorldPage(w http.ResponseWriter, r *http.Request){
+	fmt.Fprint(w, "Hello world!")
+}
+
+func main(){
+	http.HandleFunc("/", helloWorldPage)
+    log.Fatal(http.ListenAndServe(":8080", nil))
+}
